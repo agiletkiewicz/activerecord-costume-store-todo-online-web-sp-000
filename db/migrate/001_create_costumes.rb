75f1695e931add7244ff8af9fa9da5702b94e54b
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-18 07:47:39 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name 
       t.integer :price 
-      t.
+      t.integer :size 
+      t.string :image_url
+      t.timestamps
     end
     
   end
